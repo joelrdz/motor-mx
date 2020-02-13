6a0109461e5bbfeb2499d01d899f4d8c0cea3259
Joel Rdz <joelrdzh@gmail.com>
2020-02-13 02:21:39 -0600

    Making new changes for the uploaded version

--- js/main.js
+++ js/main.js
@@ -50,5 +50,41 @@ $(window).scroll(function() {
 });
 
 
+
+
+
 // Glide slider
 new Glide('#glide-exponents').mount();
+
+
+
+
+// Countdown
+
+// Set the date we're counting down to
+let countDownDate = new Date("Mar 24, 2020 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval( function() {
+	// Get today's date and time
+	let now = new Date().getTime();
+
+	// Find the distance between now and the count down date
+	let distance = countDownDate - now;
+
+	// Time calculations for days, hours, minutes and seconds
+	let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in the element with id="countdown"
+	// document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m ";
+
+	// If the count down is finished, write some text
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("countdown").innerHTML = "LLEGÓ EL DÍA";
+	}
+}, 1000);
